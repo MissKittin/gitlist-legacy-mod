174673295c6e78d10206eee7f02b82e211ccc1aa
MissKittin <56042452+MissKittin@users.noreply.github.com>
2024-06-13 17:17:16 +0200

    Assets moved to public

--- public/index.php
+++ public/index.php
@@ -13,13 +13,15 @@ if (php_sapi_name() == 'cli-server' && file_exists(substr($_SERVER['REQUEST_URI'
     return false;
 }
 
-if (!is_writable(__DIR__ . DIRECTORY_SEPARATOR . 'cache')) {
-    die(sprintf('The "%s" folder must be writable for GitList to run.', __DIR__ . DIRECTORY_SEPARATOR . 'cache'));
-}
+chdir(__DIR__.'/..');
 
 require 'vendor/autoload.php';
 
 $config = GitList\Config::fromFile('config.ini');
+
+if ($config->get('app', 'cache') === '1' && !is_writable(__DIR__ . DIRECTORY_SEPARATOR . 'cache')) {
+    die(sprintf('The "%s" folder must be writable for GitList to run.', __DIR__ . DIRECTORY_SEPARATOR . 'cache'));
+}
 
 if ($config->get('date', 'timezone')) {
     date_default_timezone_set($config->get('date', 'timezone'));
